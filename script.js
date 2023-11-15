451dcd103a8f84033e05edf1b812f9d288e2639a
zeus <zeuskeyz@gmail.com>
2023-11-16 00:43:09 +0300

    adds an algorithm that allows js to compute the nps score and display an alert for now

--- script.js
+++ script.js
@@ -1,20 +1,23 @@
-const ratings = document.querySelectorAll('.rating')
+const ratings = document.querySelectorAll('.rating');
+const agentName = document.getElementById('agentName');
 
-let ratingsArray = []
-let totalRespondents = 0
-let promoters = 0
-let passives = 0
-let detractors = 0
+let respondents = 0;
+let promoters = 0;
+let passives = 0;
+let detractors = 0;
 
 ratings.forEach(rating => {
     //adds an event listener to each button
     rating.addEventListener('click', event => {
-        totalRespondents++;
-        ratingsArray.push(event.target.innerText);
+        respondents++;
         let score = parseInt(event.target.innerText);
         score > 7 ? promoters++ : score <= 5 ? detractors++ : passives++;
-    })
-})
+    });
+});
 
+agentName.addEventListener('click', () => {
+    let NPS = (promoters - detractors) / respondents * 100;
+    return alert(`PROMOTERS : ${promoters}\nDETRACTORS : ${detractors}\nNPS SCORE : ${NPS}%origin master`);
+})
 
 
