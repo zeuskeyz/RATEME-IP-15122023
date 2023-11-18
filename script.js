5c0d17bbe0be6cf4abb8fce736f7fdb375259e90
zeus <zeuskeyz@gmail.com>
2023-11-18 08:53:47 +0300

    final commit

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 const emojis = document.querySelectorAll('.emojis');
-const agentName = document.getElementById('agentName');
+const agentScore = document.getElementById('agentScore');
 
 let respondents = 0;
 let promoters = 0;
 let passives = 0;
 let detractors = 0;
-const targetNPS = 90
+const target= 90
 
 emojis.forEach(emoji => {
     //adds an event listener to each card
@@ -15,17 +15,13 @@ emojis.forEach(emoji => {
         let service = event.target.id;
         service === 'excellent' ? promoters++ : service === 'poor' ? detractors++ : passives++
     });
-});
+}, {capture:true});
 
-agentName.addEventListener('click', (event) => {
+agentScore.addEventListener('click', (event) => {
     event.stopPropagation()
-    console.log(`promoters: ${promoters}`)
-    console.log(`detractors: ${detractors}`)
-    console.log(`passives: ${passives}`)
-    console.log(`respondents: ${respondents}`)
     let nps = 100*(promoters - detractors)/respondents
-    nps < targetNPS ? agentName.className += ' btn-danger' : nps > targetNPS ? agentName.className += ' btn-success' : agentName.className += ' btn-warning'; 
-    return agentName.innerText = (`Your NPS : ${nps}%`);
+    nps < target && nps > 0 ? agentScore.className += ' btn-danger' : nps > target ? agentScore.className += ' btn-success' : agentScore.className += ' btn-warning'; 
+    return agentScore.innerText = (`Your NPS : ${nps.toFixed(2)}%`);
 }, {capture:true})
 
 
